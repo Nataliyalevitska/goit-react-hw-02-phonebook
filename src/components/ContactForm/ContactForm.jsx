361dd7680f9bfda3bb02d0e7b0cc3ff350248f7f
natalilevitska <nataliyalevi579@gmail.com>
2022-06-30 11:42:50 +0300

    removed space

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,20 +1,25 @@
-import React, { Component } from "react";
-import { nanoid } from "nanoid";
-import { PhoneLabel,PhoneInput,ButtonContact,PhoneForm } from './ContactForm.styled';
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import {
+  PhoneLabel,
+  PhoneInput,
+  ButtonContact,
+  PhoneForm,
+} from './ContactForm.styled';
 
 class ContactForm extends Component {
   state = {
-    name: "",
-    number: "",
+    name: '',
+    number: '',
   };
-  handleChange = (e) => {
+  handleChange = e => {
     const { name, value } = e.currentTarget;
     this.setState({
       [name]: value,
     });
   };
 
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault();
     const isDuplicate = this.checkIfDuplicate();
     if (isDuplicate) {
@@ -27,23 +32,23 @@ class ContactForm extends Component {
     this.props.allContacts.some(({ name }) => name === this.state.name);
 
   reset = () => {
-    this.setState({ name: "", number: "" });
+    this.setState({ name: '', number: '' });
   };
   render() {
     const { handleChange, state, handleSubmit } = this;
     const { name, number } = state;
-    const isBtnDis = Object.values(this.state).some((value) => {
+    const isBtnDis = Object.values(this.state).some(value => {
       return !value;
     });
     return (
       <PhoneForm onSubmit={handleSubmit}>
         <PhoneLabel>
           Name
-          <PhoneInput 
+          <PhoneInput
             type="text"
             name="name"
             value={name}
-             placeholder="*Enter name"
+            placeholder="*Enter name"
             onChange={handleChange}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title=" Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan "
@@ -52,18 +57,18 @@ class ContactForm extends Component {
         </PhoneLabel>
         <PhoneLabel>
           Number
-          <PhoneInput 
+          <PhoneInput
             type="tel"
             name="number"
             value={number}
-             placeholder="*Enter phone"
+            placeholder="*Enter phone"
             onChange={handleChange}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title=" Phone number must be digits and can contain spaces, dashes, parentheses and can start with + "
             required
           />
         </PhoneLabel>
-        <ButtonContact  type="submit" disabled={isBtnDis}>
+        <ButtonContact type="submit" disabled={isBtnDis}>
           Add contact
         </ButtonContact>
       </PhoneForm>
